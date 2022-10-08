ff16660de14a95fa55f1b12554b06e4effdb5d08
akhanf <alik@robarts.ca>
2022-10-08 13:36:24 -0400

    update heuristic to capture more variants

--- resources/heuristic.py
+++ resources/heuristic.py
@@ -10,197 +10,6 @@ import logging
 lgr = logging.getLogger(__name__)
 scaninfo_suffix = '.json'
 
-T1W_SERIES = [
-    'MPRAGE 2 ADNI',
-    'MPRAGE ADNI',
-    'MPRAGE GRAPPA 2',
-    'MPRAGE GRAPPA2',
-    'MPRAGE GRAPPA2(adni)',
-    'MPRAGE w/ GRAPPA',
-    'MPRAGE_GRAPPA',
-    'MPRAGE_GRAPPA_ADNI',
-    'MPRAGE GRAPPA',
-    'SAG T1 3D MPRAGE',
-    'sag mprage',
-    'MPRAGEadni',
-    'MPRAGE GRAPPA_ND',
-    '3D SAG',
-    'MPRAGE T1 SAG',
-    'MPRAGE SAG',
-    'SAG T1 3DMPRAGE',
-    'SAG T1 MPRAGE',
-    'SAG 3D T1',
-    'SAG MPRAGE GRAPPA2-NEW2016',
-    'SAG MPRAGE GRAPPA_ND',
-    'Sag MPRAGE GRAPPA',
-    'AXIAL T1 3D MPRAGE',
-    'SAG MPRAGE GRAPPA',
-    'sT1W_3D_FFE',
-    'sT1W_3D_ISO',
-    'sT1W_3D_TFE',
-    'sag 3D FSPGR BRAVO straight',
-    'SAG T1 3D FSPGR',
-    'SAG FSPGR 3D '
-    'SAG 3D FSPGR BRAVO STRAIGHT',
-    'SAG T1 3D FSPGR 3RD REPEAT',
-    'SAG FSPGR BRAVO',
-    'SAG SPGR 3D',
-    'SAG 3D SPGR',
-    'FSPGR 3D SAG',
-    'SAG FSPGR 3D',
-    'SAG 3D FSPGR BRAVO STRAIGHT',
-    'SAG FSPGR 3D ',
-    't1_mpr_ns_sag_p2_iso',
-    'T1',
-    'T1 Repeat',
-    'AX T1',
-    'axial spgr',
-    'T1W_3D_FFE AX',
-    '3D T1-weighted',
-    'SAG 3D MPRAGE',
-    # this might have a contrast but I literally can't find any info on it
-    'AX T1 SE C+'
-]
-
-T2W_SERIES = [
-    # single echo only
-    't2_tse_tra',
-    't2 cor',
-    'T2 COR',
-    'T2W_TSE',
-    'AX T2',
-    'AX T2 AC-PC LINE ENTIRE BRAIN',
-    'AX T2 AC-PC line Entire Brain',
-    'Ax T2 Fse thin ac-pc',
-    # mixed single / dual-echo
-    'AXIAL FSE T2 FS'
-]
-
-PD_SERIES = [
-    'Ax T2* GRE'
-]
-
-PDT2_SERIES = [
-    'AX DE TSE',
-    'AX DUAL_TSE',
-    'DUAL_TSE',
-    'sT2W/PD_TSE',
-    'Axial PD-T2-FS TSE',
-    'Axial PD-T2 TSE',
-    'Axial PD-T2 TSE FS',
-    'AXIAL PD-T2 TSE FS',
-    'AX PD + T2',
-    'PD-T2 DUAL AXIAL TSE',
-    'Axial PD-T2 TSE_AC/PC line',
-    'Axial PD-T2 TSE_AC PC line',
-    'Ax PD /T2',
-    'AXIAL PD+T2 TSE',
-    'AX T2 DE',
-    't2 weighted double echo',
-    'T2'
-]
-
-FLAIR_SERIES = [
-    # FLAIR (no weighting specified)
-    'FLAIR_LongTR AX',
-    'FLAIR_LongTR SENSE',
-    'AX FLAIR',
-    'AXIAL FLAIR',
-    'FLAIR_longTR',
-    'FLAIR AXIAL',
-    'ax flair',
-    'Cor FLAIR TI_2800ms',
-    'FLAIR',
-    # T2 FLAIR
-    'AX T2 FLAIR',
-    'T2  AXIAL FLAIR',
-    'Ax T2 FLAIR  ang to ac-pc',
-    'T2W_FLAIR',
-    'AX FLAIR T2',
-    'AX T2 FLAIR 5/1',
-    'Ax T2 FLAIR',
-    't2_tirm_tra_dark-fluid_',
-    't2_tirm_tra_dark-fluid NO BLADE',
-    '3D T2 SAG FLAIR',
-    '3D T2 FLAIR',
-    # T1 FLAIR -- should these be here?
-    'Ax T1 FLAIR',
-    'AX T1 FLAIR',
-]
-
-BOLD_SERIES = [
-    'ep2d_RESTING_STATE',
-    'ep2d_bold_rest',
-    'R>L RESTING STATE FMRI ep2d_fid_basic_bold',
-    'L>R RESTING STATE FMRI ep2d_fid_basic_bold',
-    'rsfMRI_RL',
-    'rsfMRI_LR'
-]
-
-DTI_SERIES = [
-    'DTI_gated',
-    'DTI_non_gated',
-    'DTI_pulse gated_AC/PC line',
-    'REPEAT_DTI_GATED',
-    'DTI_NONGATED',
-    'REPEAT_DTI_NONGATED',
-    'TRIGGERED DTI',
-    'DTI_NON gated',
-    'DTI_ non_gated',
-    'DTI_non gated Repeat',
-    'DTI_NON-GATED',
-    'REPEAT_DTI_NON-GATED',
-    'DTI_none_gated',
-    'DTI_non gated',
-    'Repeat DTI_non gated',
-    'REPEAT_NON_GATED',
-    'DTI',
-    'REPEAT_DTI_ NON GATED',
-    'REPEAT_DTI_NON GATED',
-    'DTI_NON GATED',
-    'DTI Sequence',
-    'DTI_ NON gated REPEAT',
-    'DTI_ non gated',
-    'DTI_GATED',
-    'DTI_NON gated REPEAT',
-    'DTI_NON_GATED',
-    'DTI_Non Gated',
-    'DTI_Non gated',
-    'DTI_Non gated Repeat',
-    'DTI_Non-gated',
-    'DTI_UNgated',
-    'DTI_UNgated#2',
-    'DTI_gated AC-PC LINE',
-    'DTI_gated#1',
-    'DTI_gated#2',
-    'DTI_gated_ADC',
-    'DTI_gated_FA',
-    'DTI_gated_TRACEW',
-    'DTI_non gated repeat',
-    'DTI_pulse gated_AC PC line',
-    'DTI_ungated',
-    'REPEAT DTI_NON GATED',
-    'REPEAT DTI_NON gated',
-    'REPEAT_NON DTI_GATED',
-    'Repeat DTI Sequence',
-    'DTI_LR',
-    'DTI_RL'
-]
-
-T2W_PDT2_SERIES = [
-    'Ax T2 FSE',        # only PD/T2                        (48-65 slices)
-    '*AX FSE T2',       # mixed T2w and PD/T2               (24-64 slices)
-    'AX T2 FSE',        # only T2w (one subject)            (24-24 slices)
-    '*Ax T2 FSE',       # only T2w (one subject)            (22-22 slices)
-    'AXIAL  T2  FSE',   # only T2w                          (23-26 slices)
-]
-
-#unused right now:
-T2W_GRE_SERIES = [
-    'AX T2 GRE MT',
-    '2D GRE MT'
-]
-
 
 def create_key(template, outtype=('nii.gz',), annotation_classes=None):
     if template is None or not template:
@@ -223,62 +32,44 @@ def infotodict(seqinfo):
     # {bids_subject_session_dir} : "sub-X/ses-Y"
     # {bids_subject_session_prefix} : "sub-X_ses-Y"
     t1w = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_T1w')  # noqa
-    t1w_grappa = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-grappa2_run-{item:02d}_T1w')  # noqa
-    t1w_adni = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_acq-adni_run-{item:02d}_T1w')  # noqa
     t2w = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_T2w')  # noqa
-    pd = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_PD')  # noqa
+    t2starw = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_T2starw')  # noqa
+    pd = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_PDw')  # noqa
+    mtw = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_MTw')  # noqa
     pdt2 = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_PDT2')  # noqa
     flair = create_key('{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_FLAIR')  # noqa
     bold = create_key('{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-rest_run-{item:02d}_bold')  # noqa
     dti = create_key('{bids_subject_session_dir}/dwi/{bids_subject_session_prefix}_run-{item:02d}_dwi')  # noqa
 
-    info = {t1w: [], t1w_grappa: [], t1w_adni: [],
+    info = {t1w: [], mtw: [], t2starw: [],
             t2w: [], pd: [], pdt2: [], flair: [], bold: [], dti: []}
-    revlookup = {}
 
     for s in seqinfo:
-        revlookup[s.series_id] = s.series_description
+        seql = s.series_description.lower()
 
-        # the straightforward scan series
-        if s.series_description in T1W_SERIES:
+        if ('t1' in seql) or ('mprage' in seql) or ('spgr' in seql) or ('bravo' in seql) or ('mpr' in seql and 'sag' in seql):
             info[t1w].append(s.series_id)
-        elif s.series_description in T2W_SERIES:
-            info[t2w].append(s.series_id)
-        elif s.series_description in PD_SERIES:
-            info[pd].append(s.series_id)
-        elif s.series_description in PDT2_SERIES:
-            info[pdt2].append(s.series_id)
-        elif s.series_description in FLAIR_SERIES:
-            info[flair].append(s.series_id)
-        elif s.series_description in BOLD_SERIES:
-            info[bold].append(s.series_id)
-        elif s.series_description in DTI_SERIES:
+        elif ('dti' in seql) or ('diff' in seql) or ('gated' in seql) or ('dwi' in seql):
             info[dti].append(s.series_id)
-        # the less straightforward (mixed) series
-        elif s.series_description in T2W_PDT2_SERIES:
+        elif 'flair' in seql:
+            info[flair].append(s.series_id)
+        elif ('rs' in seql) or ('resting' in seql):
+            info[bold].append(s.series_id)
+        elif ('nm' in seql) or ('mt' in seql):
+            info[mtw].append(s.series_id)
+        elif ('pd' in seql) or ('dual' in seql):
             if s.dim3 < 40:
                 info[t2w].append(s.series_id)
             else:
                 info[pdt2].append(s.series_id)
-        # if we don't match _anything_ then we want to know!
+        elif 't2' in seql:
+            info[t2w].append(s.series_id)
+        elif ('swan' in seql) or ('*' in seql):
+            info[t2starw].append(s.series_id)
         else:
             lgr.warning('Skipping unrecognized series description: {}'
                         .format(s.series_description))
 
-    # if we have multiple t1w runs we want to add an "acq" tag to some of them
-    if len(info[t1w]) > 1:
-        # copy out t1w image series ids and reset info[t1w]
-        all_t1w = info[t1w].copy()
-        info[t1w] = []
-
-        for series_id in all_t1w:
-            series_description = revlookup[series_id].lower()
-            if series_description in ['mprage_grappa', 'sag_mprage_grappa']:
-                info[t1w].append(series_id)
-            elif 'adni' in series_description:
-                info[t1w_adni].append(series_id)
-            else:
-                info[t1w_grappa].append(series_id)
 
     return info
 
